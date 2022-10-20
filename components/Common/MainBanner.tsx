4223c01d52f77d450d7c05c8ee4ec664382ba048
Miguelangel Nuñez <migueln1@outlook.com>
2022-10-19 22:34:02 -0400

    performance image enhacements

--- components/Common/MainBanner.tsx
+++ components/Common/MainBanner.tsx
@@ -4,7 +4,13 @@ type BannerProps = {
 }
 const MainBanner = ({category}:BannerProps) => (
     <div className='w-screen h-auto mb-5 relative'>
-      <Image src="https://res.cloudinary.com/migueln1/image/upload/v1666231888/header-x1_f5ukxm.png" alt='Electronics products banner' sizes='480, 1365' width={1365} height={412} className="w-screen max-h-[22.888rem] h-full"  quality={85}/>
+      <div className='relative w-screen h-[22rem]'>
+        <Image src="https://res.cloudinary.com/migueln1/image/upload/v1666231888/header-x1_f5ukxm.png" 
+          alt={`${category} products banner`}
+          layout="fill"
+          objectFit='cover'
+          sizes='480, 1365' quality={85}/>
+      </div>
       <div className="absolute h-full w-full top-0">
         <div className="container relative w-full h-full">
           <span className='absolute bottom-[2.667rem] md:text-[4rem] text-[1.5rem] text-white select-none'>{category}</span>
